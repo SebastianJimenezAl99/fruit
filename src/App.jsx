678714974cad92d13bc3ec2bcf3753bfb0b3ad7d
Sebas <mr.sjaldana@gmail.com>
2023-09-13 12:06:27 -0500

    Primer cambio 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,24 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        {/* header */}
+      </div>
+      <div>
+        {/* Imagenes con texto 4 cuadros */}
+      </div>
+      <div>
+        {/* imagenes con texto dentro 2 cuadros */}
+      </div>
+      <div>
+        {/* testimonios */}
+      </div>
+      <div>
+        {/* LAS CUATRO IMAGENES EN FILA */}
+      </div>
+      <div>
+        {/* flooter */}
+      </div>
     </>
   )
 }
