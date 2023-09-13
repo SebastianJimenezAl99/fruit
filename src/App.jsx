b4c69b0462543aafdba20a3dac610522e67c18a9
jhoan-herrera <jsebastiahgarcia@gmail.com>
2023-09-13 12:58:12 -0500

    jhoan

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,36 @@ function App() {
         {/* header */}
       </div>
       <div>
-        {/* Imagenes con texto 4 cuadros */}
+        return (
+    <div className="container">
+      <div className="left">
+        <h1>transform your
+           brand</h1>
+        <p>We are full-servece creative agency specializing in 
+          in helping brands grow fast. Engage your clients through
+          compelling visuals thet do most of the markething for you.
+        </p>
+        <h4>LEARN MORE</h4>
+      </div>
+      <div className="right">
+        <img src="ruta_de_la_imagen.jpg" alt="Descripción de la imagen" />
+      </div>
+      <div className="clear"></div>
+      <div className="left">
+        <img src="otra_ruta_de_imagen.jpg" alt="Descripción de la imagen" />
+      </div>
+      <div className="right">
+        <h1>Stand out to the right
+           audience</h1>
+        <p>Using a collaborative formula of designers, researchers,
+          photographers, videographers and copywriters, we´ll 
+          build and extend your brand in digital places.
+        </p>
+        <h4>LEARN MORE</h4>
+      </div>
+    </div>
+  );
+
       </div>
       <div>
         {/* imagenes con texto dentro 2 cuadros */}
